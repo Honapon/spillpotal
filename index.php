288dc73b4551401a5914281a4c188b363c530b2e
Honapona_ <156892939+Honapon@users.noreply.github.com>
2024-11-05 11:13:02 +0100

    aaa

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="skjema">
+        <h1>Logg inn</h1>
+        <form action="">
+            <input name="usernaeme" type="text" placeholder="Username">
+            <input name="password" type="password" placeholder="Password">
+            <a href="register.php">register</a>
+            <button>
+                Logg inn
+            </button>
+            
+        </form>
+
+    </div>
+</body>
+
+</html>
