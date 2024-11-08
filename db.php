3c16811c4a04ffd0510adb0a135aaa2e9fe86224
Honapona_ <156892939+Honapon@users.noreply.github.com>
2024-11-08 11:10:21 +0100

    push

--- db.php
+++ db.php
@@ -1,28 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
 <?php
- 
-$dsn = "mysql:host=localhost;dbname=user_n_password";
-$username = "game";
-$password = "password";
- 
-try {
-    $connect = new PDO($dsn, $username, $password);
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo("connected to this (yipi)");
-    return $connect;
-} catch (PDOException $e) {
-    echo 'Connection no work :( </br>' . $e->getMessage();
-    exit;
+
+function dbConnect()
+{
+    try {
+        $connect = new PDO($dsn, $username, $password);
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo ("connected to this (yipi)");
+
+        //user data
+        $user_name = 'user_name';
+        $user_password = 'user_password';
+        $hashed_password = password_hash($user_password, PASSWORD_BCRYPT);
+
+        //insert user data
+        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+        $stmt = $connect->prepare($sql);
+        $stmt->bindParam(':username', $user_name);
+        $stmt->bindParam(':password', $hashed_password);
+
+        if ($stmt->execute()) {
+            echo "New Record created successfully";
+        } else {
+            echo "error: Could not execute the query";
+        }
+
+        return $connect;
+    } catch (PDOException $e) {
+        echo 'Connection no work :( </br>' . $e->getMessage();
+        exit;
+    }
 }
- 
-?>
